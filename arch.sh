8d15adad54844d5d92cf964aaf0959d3687a92d5
Dave Misbehave <dave@davemisbehave.com>
2026-01-11 22:48:38 +0100

    Improved output
    - Improved output
    - Working directory is used as destination if none is specified

--- arch.sh
+++ arch.sh
@@ -9,15 +9,25 @@ if [[ ! -e "$SOURCE_PATH" ]]; then
     exit 1
 fi
 
-DESTINATION_DIR="$2"
+if (( $# >= 2 )); then
+    if [[ -n "$2" ]]; then
+		DESTINATION_DIR="$2"
+	else
+		DESTINATION_DIR="$PWD"
+	fi
+else
+	DESTINATION_DIR="$PWD"
+fi
+DESTINATION_DIR=${DESTINATION_DIR:a}
 
 printf "Source:\t\t%s\n" "$SOURCE_PATH"
 DESTINATION_FILE="${DESTINATION_DIR:a}/${SOURCE_PATH:t}.tar.7z"
-echo "Determining Source Size..."
+printf "Destination:\t%s\n" "$DESTINATION_FILE"
+printf "Determining Source Size..."
 SOURCE_SIZE=$(du -sh "$SOURCE_PATH" | cut -f1)
 SOURCE_SIZE_BYTE=$(du -sk "$SOURCE_PATH" | awk '{print $1 * 1024}')
-echo "Source Size: $SOURCE_SIZE / $SOURCE_SIZE_BYTE bytes"
-printf "Destination:\t%s\n" "$DESTINATION_FILE"
+tput cr && tput el
+printf "\rSource Size: $SOURCE_SIZE / $SOURCE_SIZE_BYTE bytes\n"
 read "?Confirm with 'y': " CONFIRMATION
 [[ $CONFIRMATION == "y" ]] || {
     echo "Exiting."
@@ -26,12 +36,11 @@ read "?Confirm with 'y': " CONFIRMATION
 
 tar --acls --xattrs -C "${SOURCE_PATH:h}" -cf - "${SOURCE_PATH:t}" 2>/dev/null | 7zz a -t7z -si -mx=9 -m0=lzma2 -md=256m -mmt=on -bsp1 "$DESTINATION_FILE"
 
-echo "Determining Archive Size..."
+printf "Determining Archive Size..."
 ARCHIVE_SIZE=$(du -sh "$DESTINATION_FILE" | cut -f1)
 ARCHIVE_SIZE_BYTE=$(du -sk "$DESTINATION_FILE" | awk '{print $1 * 1024}')
-echo "Archive Size: $ARCHIVE_SIZE / $ARCHIVE_SIZE_BYTE bytes"
-
 PERCENTAGE=$(( (ARCHIVE_SIZE_BYTE * 100.0) / SOURCE_SIZE_BYTE ))
-printf "%.1f%%\n" "$PERCENTAGE"
+tput cr && tput el
+printf "\rArchive Size: $ARCHIVE_SIZE / $ARCHIVE_SIZE_BYTE bytes (%.1f%%)\n" "$PERCENTAGE"
 
 echo "klolthxbye"
--- unarch.sh
+++ unarch.sh
@@ -9,29 +9,33 @@ if [[ ! -e "$SOURCE_PATH" ]]; then
     exit 1
 fi
 
-if [[ -n "$2" ]]; then
-    DESTINATION_DIR="$2"
+if (( $# >= 2 )); then
+    if [[ -n "$2" ]]; then
+		DESTINATION_DIR="$2"
+	else
+		DESTINATION_DIR="$PWD"
+	fi
 else
-    DESTINATION_DIR="$PWD"
+	DESTINATION_DIR="$PWD"
 fi
 DESTINATION_DIR=${DESTINATION_DIR:a}
 
-
-printf "Source:\t\t%s\n" "$SOURCE_PATH"
-printf "Destination:\t%s\n" "$DESTINATION_DIR"
-
+printf "Source:\t\t\t%s\n" "$SOURCE_PATH"
+printf "Destination:\t\t%s\n" "$DESTINATION_DIR"
+printf "Determining Source Size..."
 SOURCE_SIZE=$(du -sh "$SOURCE_PATH" | cut -f1)
 SOURCE_SIZE_BYTE=$(du -sk "$SOURCE_PATH" | awk '{print $1 * 1024}')
-echo "Source Size: $SOURCE_SIZE / $SOURCE_SIZE_BYTE bytes"
+tput cr && tput el
+printf "\rSource Size:\t\t$SOURCE_SIZE / $SOURCE_SIZE_BYTE bytes\n"
 mkdir -p "$DESTINATION_DIR"
-echo "Decompressing..."
+printf "Decompressing..."
 7zz x -so "$SOURCE_PATH" | tar --acls --xattrs -C "$DESTINATION_DIR" -xf -
-
+tput cr && tput el
+printf "\rDetermining Destination Size..."
 DESTINATION_SIZE=$(du -sh "$DESTINATION_DIR" | cut -f1)
 DESTINATION_SIZE_BYTE=$(du -sk "$DESTINATION_DIR" | awk '{print $1 * 1024}')
-echo "Destination Size: $SOURCE_SIZE / $SOURCE_SIZE_BYTE bytes"
-
 PERCENTAGE=$(( (DESTINATION_SIZE_BYTE * 100.0) / SOURCE_SIZE_BYTE ))
-printf "%.1f%%\n" "$PERCENTAGE"
+tput cr && tput el
+printf "\rDestination Size:\t$DESTINATION_SIZE / $DESTINATION_SIZE_BYTE bytes (%.1f%%)\n" "$PERCENTAGE"
 
 echo "klolthxbye"
