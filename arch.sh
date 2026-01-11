8264fafd98e7a5c6562dffdb99af51615f749211
Dave Misbehave <dave@davemisbehave.com>
2026-01-12 00:01:46 +0100

    Difference shown
    - Difference in size is now shown
    - Human readable sizes are unified in style

--- arch.sh
+++ arch.sh
@@ -2,6 +2,21 @@
 
 set -uo pipefail
 
+to_human() {
+	if (( "$1" < 0 )); then
+		local ABS_SIZE_BYTES=$(( "$1" * -1 ))
+		printf "-"
+	else
+		local ABS_SIZE_BYTES="$1"
+	fi
+    echo $ABS_SIZE_BYTES | awk '{
+        split("B KB MB GB TB", unit);
+        i=1;
+        while($1>=1024 && i<5) { $1/=1024; i++ }
+        printf "%.1f %s", $1, unit[i]
+    }'
+}
+
 SOURCE_PATH="$1"
 
 if [[ ! -e "$SOURCE_PATH" ]]; then
@@ -27,7 +42,7 @@ printf "Determining Source Size..."
 SOURCE_SIZE=$(du -sh "$SOURCE_PATH" | cut -f1)
 SOURCE_SIZE_BYTE=$(du -sk "$SOURCE_PATH" | awk '{print $1 * 1024}')
 tput cr && tput el
-printf "\rSource Size: $SOURCE_SIZE / $SOURCE_SIZE_BYTE bytes\n"
+printf "\rSource Size:\t$SOURCE_SIZE / $SOURCE_SIZE_BYTE bytes\n"
 read "?Confirm with 'y': " CONFIRMATION
 [[ $CONFIRMATION == "y" ]] || {
     echo "Exiting."
@@ -41,6 +56,10 @@ ARCHIVE_SIZE=$(du -sh "$DESTINATION_FILE" | cut -f1)
 ARCHIVE_SIZE_BYTE=$(du -sk "$DESTINATION_FILE" | awk '{print $1 * 1024}')
 PERCENTAGE=$(( (ARCHIVE_SIZE_BYTE * 100.0) / SOURCE_SIZE_BYTE ))
 tput cr && tput el
-printf "\rArchive Size: $ARCHIVE_SIZE / $ARCHIVE_SIZE_BYTE bytes (%.1f%%)\n" "$PERCENTAGE"
+printf "\rArchive Size:\t$ARCHIVE_SIZE / $ARCHIVE_SIZE_BYTE bytes (%.1f%%)\n" "$PERCENTAGE"
+SIZE_DIFFERENCE_BYTE=$(( ARCHIVE_SIZE_BYTE - SOURCE_SIZE_BYTE ))
+SIZE_DIFFERENCE=$(to_human $SIZE_DIFFERENCE_BYTE)
+
+printf "Difference:\t$SIZE_DIFFERENCE / $SIZE_DIFFERENCE_BYTE bytes\n"
 
 echo "klolthxbye"
--- unarch.sh
+++ unarch.sh
@@ -2,6 +2,21 @@
 
 set -uo pipefail
 
+to_human() {
+	if (( "$1" < 0 )); then
+		local ABS_SIZE_BYTES=$(( "$1" * -1 ))
+		printf "-"
+	else
+		local ABS_SIZE_BYTES="$1"
+	fi
+    echo $ABS_SIZE_BYTES | awk '{
+        split("B KB MB GB TB", unit);
+        i=1;
+        while($1>=1024 && i<5) { $1/=1024; i++ }
+        printf "%.1f %s", $1, unit[i]
+    }'
+}
+
 SOURCE_PATH="$1"
 
 if [[ ! -e "$SOURCE_PATH" ]]; then
@@ -23,8 +38,8 @@ DESTINATION_DIR=${DESTINATION_DIR:a}
 printf "Source:\t\t\t%s\n" "$SOURCE_PATH"
 printf "Destination:\t\t%s\n" "$DESTINATION_DIR"
 printf "Determining Source Size..."
-SOURCE_SIZE=$(du -sh "$SOURCE_PATH" | cut -f1)
 SOURCE_SIZE_BYTE=$(du -sk "$SOURCE_PATH" | awk '{print $1 * 1024}')
+SOURCE_SIZE=$(to_human $SOURCE_SIZE_BYTE)
 tput cr && tput el
 printf "\rSource Size:\t\t$SOURCE_SIZE / $SOURCE_SIZE_BYTE bytes\n"
 mkdir -p "$DESTINATION_DIR"
@@ -32,10 +47,13 @@ printf "Decompressing..."
 7zz x -so "$SOURCE_PATH" | tar --acls --xattrs -C "$DESTINATION_DIR" -xf -
 tput cr && tput el
 printf "\rDetermining Destination Size..."
-DESTINATION_SIZE=$(du -sh "$DESTINATION_DIR" | cut -f1)
 DESTINATION_SIZE_BYTE=$(du -sk "$DESTINATION_DIR" | awk '{print $1 * 1024}')
+DESTINATION_SIZE=$(to_human $DESTINATION_SIZE_BYTE)
 PERCENTAGE=$(( (DESTINATION_SIZE_BYTE * 100.0) / SOURCE_SIZE_BYTE ))
 tput cr && tput el
 printf "\rDestination Size:\t$DESTINATION_SIZE / $DESTINATION_SIZE_BYTE bytes (%.1f%%)\n" "$PERCENTAGE"
+SIZE_DIFFERENCE_BYTE=$(( DESTINATION_SIZE_BYTE - SOURCE_SIZE_BYTE ))
+SIZE_DIFFERENCE=$(to_human $SIZE_DIFFERENCE_BYTE)
+printf "Difference:\t\t$SIZE_DIFFERENCE / $SIZE_DIFFERENCE_BYTE bytes\n"
 
 echo "klolthxbye"
